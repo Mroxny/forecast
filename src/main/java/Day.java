d5284b1eb1832233ee90261d1d25b4a872d4dbec
Mroxny <50219282+Mroxny@users.noreply.github.com>
2022-12-02 20:44:45 +0100

    Create Day.java

---
+++ src/main/java/Day.java
@@ -0,0 +1,38 @@
+public class Day{
+    private float minTemp;
+    private float maxTemp;
+    private float avgTemp;
+    private float probOfPerc;
+
+    public float getMinTemp() {
+        return minTemp;
+    }
+
+    public void setMinTemp(float minTemp) {
+        this.minTemp = minTemp;
+    }
+
+    public float getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(float maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    public float getAvgTemp() {
+        return avgTemp;
+    }
+
+    public void setAvgTemp(float avgTemp) {
+        this.avgTemp = avgTemp;
+    }
+
+    public float getProbOfPerc() {
+        return probOfPerc;
+    }
+
+    public void setProbOfPerc(float probOfPerc) {
+        this.probOfPerc = probOfPerc;
+    }
+}
