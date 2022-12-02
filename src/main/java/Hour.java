0e3a1e352275ce7ec9f4607bb77f9e651c85bb6a
Mroxny <50219282+Mroxny@users.noreply.github.com>
2022-12-02 20:48:37 +0100

    Create Hour.java

---
+++ src/main/java/Hour.java
@@ -0,0 +1,30 @@
+public class Hour {
+    private String date;
+    private float temp;
+    private float precipitation;
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public float getTemp() {
+        return temp;
+    }
+
+    public void setTemp(float temp) {
+        this.temp = temp;
+    }
+
+    public float getPrecipitation() {
+        return precipitation;
+    }
+
+    public void setPrecipitation(float precipitation) {
+        this.precipitation = precipitation;
+    }
+}
