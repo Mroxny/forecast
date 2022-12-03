0fc9664b001f3e7e6dfe9e3a2e3b2c33ad5da3c7
Mroxny <50219282+Mroxny@users.noreply.github.com>
2022-12-03 19:07:02 +0100

    Update ForecastParser.java

--- src/main/java/ForecastParser.java
+++ src/main/java/ForecastParser.java
@@ -1,6 +1,35 @@
+import org.json.simple.JSONObject;
+
 public class ForecastParser {
+
     public enum ForecastType{
         DAILY,
         HOURLY
     }
+    
+    private JSONObject jObject;
+    private ForecastType fType;
+
+
+    public ForecastParser(JSONObject jObject, ForecastType fType) {
+        this.jObject = jObject;
+        this.fType = fType;
+    }
+
+    public JSONObject getObject() {
+        return jObject;
+    }
+
+    public void setObject(JSONObject jObject) {
+        this.jObject = jObject;
+    }
+
+    public ForecastType getType() {
+        return fType;
+    }
+
+    public void setType(ForecastType fType) {
+        this.fType = fType;
+    }
+
 }
