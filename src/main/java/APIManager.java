1b28a68b876c0da169d2a333682a7db56dd13676
Mroxny <50219282+Mroxny@users.noreply.github.com>
2022-12-03 13:41:48 +0100

    Create API Manager

---
+++ src/main/java/APIManager.java
@@ -0,0 +1,123 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class APIManager {
+    private static final String URL_PREFIX = "https://api.openweathermap.org/data/2.5/onecall?";
+
+    private String key;
+    private double lat;
+    private double lon;
+    private String units;
+    private String type;
+
+
+    public APIManager(String key,double lat, double lon, String type) {
+        this.key = key;
+        this.lat = lat;
+        this.lon = lon;
+        this.type = type;
+        this.units = "metric";
+    }
+
+    public String getLat() {
+        return "lat="+lat;
+    }
+
+    public String getLon() {
+        return "lon="+lon;
+    }
+
+    public String getKey() {
+        return "appid="+key;
+    }
+
+    public String getUnits() {
+        return "units="+units;
+    }
+
+    public String getExcluded(){
+        String[] typesToExclude = {"current","minutely","hourly","daily","alerts"};
+        List<String> list = new ArrayList<String>(Arrays.asList(typesToExclude));
+        list.remove(type);
+        typesToExclude = list.toArray(new String[0]);
+
+        return "exclude="+String.join(",",typesToExclude);
+    }
+
+    public JSONObject getJSONFromString(String data){
+        try{
+            JSONParser parser = new JSONParser();
+            Object parsedObject = parser.parse(data);
+
+
+            return (JSONObject) parsedObject;
+        }
+        catch (ParseException e) {
+            Main.printError("Invalid API response");
+            return null;
+        }
+    }
+
+
+    public String getResponse(){
+        String apiRequest = URL_PREFIX+String.join("&",getLat(), getLon(), getExcluded(), getUnits(), getKey());
+        try {
+            URL url = new URL(apiRequest);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            int responseCode = connection.getResponseCode();
+
+            if(responseCode != 200){
+                Main.printError("HTTP response code "+responseCode);
+                return null;
+            }
+            else{
+                StringBuilder data = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream(), StandardCharsets.UTF_8.toString());
+
+                while (scanner.hasNext()) {
+                    data.append(scanner.nextLine());
+                }
+                scanner.close();
+
+                return String.valueOf(data);
+            }
+        } catch (IOException e) {
+           Main.printError("Can't connect to the URL");
+           return null;
+        }
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setLat(int lat) {
+        this.lat = lat;
+    }
+
+    public void setLon(int lon) {
+        this.lon = lon;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,58 +1,21 @@
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
-
-public class main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("You have "+args.length+" arguments");
 
-        // test API URL: https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=5e25fd264a5455963a0f0ee4f0a6d43e
 
-        try {
-            URL url = new URL("https://api.openweathermap.org/data/2.5/onecall?lat=52.2297&lon=21.0122&exclude=current,minutely,hourly,alerts&units=metric&appid=5e25fd264a5455963a0f0ee4f0a6d43e");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.connect();
+        APIManager apiManager = new APIManager("5e25fd264a5455963a0f0ee4f0a6d43e",33.44, 94.04, "daily");
+        String response = apiManager.getResponse();
+        JSONObject mainObject = apiManager.getJSONFromString(response);
 
-            int responseCode = connection.getResponseCode();
+        JSONArray testArray = (JSONArray) mainObject.get("daily");
 
-            if(responseCode != 200) printError("HTTP response code "+responseCode);
-            else{
-                StringBuilder informationString = new StringBuilder();
-                Scanner scanner = new Scanner(url.openStream(), StandardCharsets.UTF_8.toString());
-
-                while (scanner.hasNext()) {
-                    informationString.append(scanner.nextLine());
-                }
-                scanner.close();
-
-                JSONParser parser = new JSONParser();
-                Object parsedObject = parser.parse(String.valueOf(informationString));
-                JSONObject jsonObject = (JSONObject) parsedObject;
-
-                System.out.println(jsonObject.get("timezone"));
-
-                JSONArray testArray = (JSONArray) jsonObject.get("daily");
-
-                for(Object o: testArray){
-                    JSONObject testObj = (JSONObject) o;
-                    System.out.println(testObj.get("temp"));
-                }
-
+            for(Object o: testArray){
+                JSONObject testObj = (JSONObject) o;
+                System.out.println(testObj.get("temp"));
             }
-        } catch (IOException e) {
-            printError("Can't connect to the URL");
-        } catch (ParseException e) {
-            printError("Cannot parse JSON info");
-
-        }
 
     }
 
