0e353ed982e9439bc280daf30c8495e029bc9734
Mroxny <50219282+Mroxny@users.noreply.github.com>
2022-12-04 09:13:07 +0100

    Update ConfigReader.java

--- src/main/java/ConfigReader.java
+++ src/main/java/ConfigReader.java
@@ -1,2 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
 public class ConfigReader {
+    public static boolean containsCity(String path, String cityName) {
+        try {
+            File file = new File("Student.txt");
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.contains(cityName)) return true;
+            }
+            return false;
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+    }
+
 }
